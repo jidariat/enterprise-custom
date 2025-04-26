6e027e60a989f5788edd4c02cafa4fc08cf9d4a4
jidariat <jidariat.odoo@gmail.com>
2025-04-26 13:55:56 +0300

    add new changes in vat invoice and sale order

---
+++ quotations_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Quotation Template',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': "",
+    'description': "",
+    'author': 'Ibrahim Rizeq',
+    'company': 'jidariat Company',
+    'depends': ['base', 'sale_management'],
+    'data': [
+        # 'report/action_report.xml',
+        'views/quotation_report_custom.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
